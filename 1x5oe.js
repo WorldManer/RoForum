4f62a5ecd28d26495452036718a3dcebbd16afaa
WorldManer <worldmaner2@gmail.com>
2026-05-16 21:53:27 +0300

    Create 1x5oe.js

---
+++ 1x5oe.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://frjbgsscefunyoygxnax.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_3wP-ypYyS1sp9ifNuvr8LQ_q6NAGum3';
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
